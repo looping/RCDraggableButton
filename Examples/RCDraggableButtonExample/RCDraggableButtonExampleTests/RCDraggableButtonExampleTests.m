173c955e3e541e70605633b4793b27ff66297f7a
Looping <www.looping@gmail.com>
2014-02-11 23:46:25 +0800

    Test: Add test cases of some remove methods.

--- Examples/RCDraggableButtonExample/RCDraggableButtonExampleTests/RCDraggableButtonExampleTests.m
+++ Examples/RCDraggableButtonExample/RCDraggableButtonExampleTests/RCDraggableButtonExampleTests.m
@@ -23,10 +23,12 @@
     [super tearDown];
 }
 
+#pragma mark common
 - (void)testVersion {
     XCTAssertEqualObjects(RC_DB_VERSION, [RCDraggableButton version]);
 }
 
+#pragma mark init
 - (void)testInitInKeyWindow {
     RCDraggableButton *draggableButton = [[RCDraggableButton alloc] initInKeyWindowWithFrame:CGRectMake(120, 120, 60, 60)];
     
@@ -44,6 +46,7 @@
     XCTAssertEqualObjects(customView, [draggableButton superview]);
 }
 
+#pragma mark remove
 - (void)testRemoveFromView {
     UIView *customView = [[UIView alloc] initWithFrame:CGRectMake(10, 100, 300, 300)];
     [customView setBackgroundColor:[UIColor colorWithWhite:0.1 alpha:1]];
@@ -57,4 +60,57 @@
     
     XCTAssertEqualObjects(NULL, [draggableButton superview]);
 }
+
+- (void)testRemoveFromViewWithTag {
+    UIView *customView = [[UIView alloc] initWithFrame:CGRectMake(10, 100, 300, 300)];
+    [customView setBackgroundColor:[UIColor colorWithWhite:0.1 alpha:1]];
+    
+    RCDraggableButton *draggableButton = [[RCDraggableButton alloc] initInView:customView WithFrame:CGRectMake(120, 120, 60, 60)];
+    [draggableButton setTag:89];
+    
+    RCDraggableButton *tmpDraggableButton = [[RCDraggableButton alloc] initInView:customView WithFrame:CGRectMake(120, 120, 60, 60)];
+
+    XCTAssertEqualObjects([UIButton class], [draggableButton superclass]);
+    XCTAssertEqualObjects(customView, [draggableButton superview]);
+    
+    XCTAssertEqualObjects([UIButton class], [tmpDraggableButton superclass]);
+    XCTAssertEqualObjects(customView, [tmpDraggableButton superview]);
+    
+    [RCDraggableButton removeFromView:customView withTag:89];
+    
+    XCTAssertEqualObjects(NULL, [draggableButton superview]);
+    
+    XCTAssertEqualObjects(customView, [tmpDraggableButton superview]);
+}
+
+- (void)testRemoveFromViewWithTags {
+    UIView *customView = [[UIView alloc] initWithFrame:CGRectMake(10, 100, 300, 300)];
+    [customView setBackgroundColor:[UIColor colorWithWhite:0.1 alpha:1]];
+    
+    RCDraggableButton *draggableButton1 = [[RCDraggableButton alloc] initInView:customView WithFrame:CGRectMake(120, 120, 60, 60)];
+    [draggableButton1 setTag:89];
+    
+    XCTAssertEqualObjects([UIButton class], [draggableButton1 superclass]);
+    XCTAssertEqualObjects(customView, [draggableButton1 superview]);
+    
+    RCDraggableButton *draggableButton2 = [[RCDraggableButton alloc] initInView:customView WithFrame:CGRectMake(120, 120, 60, 60)];
+    [draggableButton2 setTag:90];
+    
+    XCTAssertEqualObjects([UIButton class], [draggableButton2 superclass]);
+    XCTAssertEqualObjects(customView, [draggableButton2 superview]);
+    
+    RCDraggableButton *tmpDraggableButton = [[RCDraggableButton alloc] initInView:customView WithFrame:CGRectMake(120, 120, 60, 60)];
+    
+    XCTAssertEqualObjects([UIButton class], [tmpDraggableButton superclass]);
+    XCTAssertEqualObjects(customView, [tmpDraggableButton superview]);
+    
+    [RCDraggableButton removeFromView:customView withTags:@[@(89), @(90)]];
+    
+    XCTAssertEqualObjects(NULL, [draggableButton1 superview]);
+    
+    XCTAssertEqualObjects(NULL, [draggableButton2 superview]);
+    
+    XCTAssertEqualObjects(customView, [tmpDraggableButton superview]);
+}
+
 @end
